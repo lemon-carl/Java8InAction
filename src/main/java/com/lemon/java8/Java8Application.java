d593a95cb957158ce8be0ce38534369d1a2a7ac4
CarlLemon <LingHuan5593@qq.com>
2021-08-23 21:04:30 +0800

    java8 init

---
+++ src/main/java/com/lemon/java8/Java8Application.java
@@ -0,0 +1,18 @@
+package com.lemon.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lemon
+ * java 8 study
+ *
+ */
+@SpringBootApplication
+public class Java8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8Application.class, args);
+    }
+
+}
